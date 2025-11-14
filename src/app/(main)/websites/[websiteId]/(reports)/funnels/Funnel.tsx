dfc16c8a03a82cff10f2d6ac4cea95c7aa50d403
Sajid Mehmood <sajid@niteshift.dev>
2025-11-14 16:35:43 +0000

    fix: change funnel dropoff percentage text weight to light

--- src/app/(main)/websites/[websiteId]/(reports)/funnels/Funnel.tsx
+++ src/app/(main)/websites/[websiteId]/(reports)/funnels/Funnel.tsx
@@ -101,7 +101,7 @@ export function Funnel({ id, name, type, parameters, websiteId }) {
                         <ChangeLabel value={-dropped} title={`${Math.round(dropoff * 100)}%`}>
                           <Row gap="2">
                             <Text>{formatLongNumber(dropped)}</Text>
-                            <Text weight="normal">({Math.round(dropoff * 100)}%)</Text>
+                            <Text weight="light">({Math.round(dropoff * 100)}%)</Text>
                           </Row>
                         </ChangeLabel>
                       )}
