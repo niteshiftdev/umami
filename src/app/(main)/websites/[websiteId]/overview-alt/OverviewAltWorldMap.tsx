c31ece1257899d425bf4de7644289fd6181ad1d7
Sajid Mehmood <sajid@niteshift.dev>
2025-11-18 23:57:17 -0500

    chore: remove websites overview alt

--- src/app/(main)/websites/[websiteId]/overview-alt/OverviewAltWorldMap.tsx
+++
@@ -1,5 +0,0 @@
-import { WorldMap } from '@/components/metrics/WorldMap';
-
-export function OverviewAltWorldMap({ websiteId }: { websiteId: string }) {
-  return <WorldMap websiteId={websiteId} />;
-}
