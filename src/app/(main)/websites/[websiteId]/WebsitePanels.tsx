18db6b172b42ed59f944d5f56a57fcbe1ff5717a
Sajid Mehmood <sajid@niteshift.dev>
2025-11-20 16:19:48 +0000

    remove: delete location panel from website metrics

--- src/app/(main)/websites/[websiteId]/WebsitePanels.tsx
+++ src/app/(main)/websites/[websiteId]/WebsitePanels.tsx
@@ -82,26 +82,6 @@ export function WebsitePanels({ websiteId }: { websiteId: string }) {
             </TabPanel>
           </Tabs>
         </Panel>
-
-        <Panel>
-          <Heading size="2">{formatMessage(labels.location)}</Heading>
-          <Tabs>
-            <TabList>
-              <Tab id="country">{formatMessage(labels.countries)}</Tab>
-              <Tab id="region">{formatMessage(labels.regions)}</Tab>
-              <Tab id="city">{formatMessage(labels.cities)}</Tab>
-            </TabList>
-            <TabPanel id="country">
-              <MetricsTable type="country" title={formatMessage(labels.country)} {...tableProps} />
-            </TabPanel>
-            <TabPanel id="region">
-              <MetricsTable type="region" title={formatMessage(labels.region)} {...tableProps} />
-            </TabPanel>
-            <TabPanel id="city">
-              <MetricsTable type="city" title={formatMessage(labels.city)} {...tableProps} />
-            </TabPanel>
-          </Tabs>
-        </Panel>
       </GridRow>
 
       <GridRow layout="two" {...rowProps}>
