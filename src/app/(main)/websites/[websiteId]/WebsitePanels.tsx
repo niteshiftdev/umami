1289884a28bb5e0f5da21366b10cd0eb297e9a83
Sajid Mehmood <sajid@niteshift.dev>
2025-11-26 21:16:14 +0000

    refactor: move WorldMap component to main WebsitePage layout

--- src/app/(main)/websites/[websiteId]/WebsitePanels.tsx
+++ src/app/(main)/websites/[websiteId]/WebsitePanels.tsx
@@ -128,11 +128,7 @@ export function WebsitePanels({ websiteId }: { websiteId: string }) {
         </Panel>
       </GridRow>
 
-      <GridRow layout="two-one" {...rowProps}>
-        <Panel gridColumn={{ xs: 'span 1', md: 'span 2' }} paddingX="0" paddingY="0">
-          <WorldMap websiteId={websiteId} />
-        </Panel>
-
+      <GridRow layout="one" {...rowProps}>
         <Panel>
           <Heading size={typography.sectionHeadingSize as any} style={headingStyle}>
             {formatMessage(labels.traffic)}
