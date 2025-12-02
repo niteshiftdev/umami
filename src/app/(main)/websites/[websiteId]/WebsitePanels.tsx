36066bb1a61cfd79c8e3bd9f5aef74405db7e9e4
Conor Branagan <472446+conorbranagan@users.noreply.github.com>
2025-12-02 21:04:25 +0000

    feat: move WorldMap component to main dashboard layout

--- src/app/(main)/websites/[websiteId]/WebsitePanels.tsx
+++ src/app/(main)/websites/[websiteId]/WebsitePanels.tsx
@@ -4,7 +4,6 @@ import { useMessages, useNavigation } from '@/components/hooks';
 import { EventsChart } from '@/components/metrics/EventsChart';
 import { MetricsTable } from '@/components/metrics/MetricsTable';
 import { WeeklyTraffic } from '@/components/metrics/WeeklyTraffic';
-import { WorldMap } from '@/components/metrics/WorldMap';
 import { Grid, Heading, Row, Tab, TabList, TabPanel, Tabs } from '@umami/react-zen';
 import { useContext } from 'react';
 import { TypographyContext } from './WebsitePage';
@@ -128,19 +127,13 @@ export function WebsitePanels({ websiteId }: { websiteId: string }) {
         </Panel>
       </GridRow>
 
-      <GridRow layout="two-one" {...rowProps}>
-        <Panel gridColumn={{ xs: 'span 1', md: 'span 2' }} paddingX="0" paddingY="0">
-          <WorldMap websiteId={websiteId} />
-        </Panel>
-
-        <Panel>
-          <Heading size={typography.sectionHeadingSize as any} style={headingStyle}>
-            {formatMessage(labels.traffic)}
-          </Heading>
-          <Row border="bottom" marginBottom="4" />
-          <WeeklyTraffic websiteId={websiteId} />
-        </Panel>
-      </GridRow>
+      <Panel>
+        <Heading size={typography.sectionHeadingSize as any} style={headingStyle}>
+          {formatMessage(labels.traffic)}
+        </Heading>
+        <Row border="bottom" marginBottom="4" />
+        <WeeklyTraffic websiteId={websiteId} />
+      </Panel>
       {isSharePage && (
         <GridRow layout="two-one" {...rowProps}>
           <Panel>
