ab6ab81112295a1bbcf364ac4704a4303916eee7
KC Beard <30806787+kc-beard@users.noreply.github.com>
2025-12-08 22:17:01 +0000

    Auto-checkpoint: code changes

--- src/app/(main)/websites/[websiteId]/WebsitePanels.tsx
+++ src/app/(main)/websites/[websiteId]/WebsitePanels.tsx
@@ -37,6 +37,12 @@ export function WebsitePanels({ websiteId }: { websiteId: string }) {
 
   return (
     <Grid gap="3">
+      <GridRow layout="two-one" {...rowProps}>
+        <Panel gridColumn={{ xs: 'span 1', md: 'span 3' }} paddingX="0" paddingY="0">
+          <WorldMap websiteId={websiteId} />
+        </Panel>
+      </GridRow>
+
       <GridRow layout="two" {...rowProps}>
         <Panel>
           <Heading size={typography.sectionHeadingSize as any} style={headingStyle}>
@@ -128,11 +134,7 @@ export function WebsitePanels({ websiteId }: { websiteId: string }) {
         </Panel>
       </GridRow>
 
-      <GridRow layout="two-one" {...rowProps}>
-        <Panel gridColumn={{ xs: 'span 1', md: 'span 2' }} paddingX="0" paddingY="0">
-          <WorldMap websiteId={websiteId} />
-        </Panel>
-
+      <GridRow layout="two" {...rowProps}>
         <Panel>
           <Heading size={typography.sectionHeadingSize as any} style={headingStyle}>
             {formatMessage(labels.traffic)}
